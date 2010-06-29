4f3146b72196611db2bd2b26524a83cd02af96d5
Dave Rolsky <autarch@urth.org>
2010-06-28 20:51:20 -0500

    Finish up pod

--- lib/Dist/Zilla/Plugin/PurePerlTests.pm
+++ lib/Dist/Zilla/Plugin/PurePerlTests.pm
@@ -68,6 +68,13 @@ __PACKAGE__->meta()->make_immutable();
 
 # ABSTRACT: Run all your tests twice, once with XS code and once with pure Perl
 
+__END__
+
+=pod
+
+=for Pod::Coverage
+  gather_files
+
 =head1 SYNOPSIS
 
 In your F<dist.ini>:
@@ -98,4 +105,28 @@ a comment like this in your test:
 This must be on a line by itself. This plugin will skip any tests which
 contain this comment.
 
+=head1 SUPPORT
 
+Please report any bugs or feature requests to
+C<bug-dist-zilla-plugin-pureperltests@rt.cpan.org>, or through the web
+interface at L<http://rt.cpan.org>. I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 DONATIONS
+
+If you'd like to thank me for the work I've done on this module, please
+consider making a "donation" to me via PayPal. I spend a lot of free time
+creating free software, and would appreciate any support you'd care to offer.
+
+Please note that B<I am not suggesting that you must do this> in order for me
+to continue working on this particular software. I will continue to do so,
+inasmuch as I have in the past, for as long as it interests me.
+
+Similarly, a donation made in this way will probably not make me work on this
+software much more, unless I get so many donations that I can consider working
+on free software full time, which seems unlikely at best.
+
+To donate, log into PayPal and send money to autarch@urth.org or use the
+button on this page: L<http://www.urth.org/~autarch/fs-donation.html>
+
+=cut
