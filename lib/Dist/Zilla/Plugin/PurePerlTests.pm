5617fb0afe4b22388ab46a56fa708c8aca1407da
Dave Rolsky <autarch@urth.org>
2011-11-09 23:19:52 -0600

    Fix skip_all message

--- lib/Dist/Zilla/Plugin/PurePerlTests.pm
+++ lib/Dist/Zilla/Plugin/PurePerlTests.pm
@@ -51,7 +51,7 @@ use Test::More;
 
 BEGIN {
     unless ( \$ENV{RELEASE_TESTING} ) {
-        plan skip_all => 'these tests are for testing by the release';
+        plan skip_all => 'these tests are for release testing';
     }
 
     \$ENV{$env_var} = 1;
